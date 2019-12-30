89a5926011b7c65ec4fe8465f036f764a7198405
Julian Steinmann <steinmann.julian@gmail.com>
2019-12-30 19:11:55 +0000

    Add buttons with click events

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,17 +1,26 @@
+//= require jquery
+//= require jquery_ujs
 
-console.log("test2");
+$(document).ready(function() {
 
+console.log("louded");
+const mpcKeys = document.querySelectorAll(".mpc-key");
+console.log( mpcKeys );
+console.log( mpcKeys.length );
 
+for (let key of mpcKeys) {
+  key.addEventListener("click", function(){
+   alert("yay");
+  });
+}
 
 function setup() {
   createCanvas(640, 480);
+
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+
 }
+
+});
